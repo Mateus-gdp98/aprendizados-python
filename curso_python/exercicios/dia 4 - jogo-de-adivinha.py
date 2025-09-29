b185e0ca2bb719a42fab667d8992a3ba8cf32d53
Mateus-gdp98 <mateusgomes1711@gmail.com>
2025-09-29 16:57:04 -0300

    Atualizando

---
+++ curso_python/exercicios/dia 4 - jogo-de-adivinha.py
@@ -0,0 +1,40 @@
+'''
+O programa escolhe um número secreto, aleatório.
+O jogador precisa tentar adivinhar esse número digitando palpites.
+A cada tentativa, o programa dá uma dica:
+Se o palpite for menor que o número secreto → mostra "Tente um número maior".
+Se o palpite for maior → mostra "Tente um número menor".
+O jogo continua em um loop até o jogador acertar.
+Quando acerta, o programa mostra uma mensagem de vitória e pode informar também quantas tentativas foram necessárias.
+'''
+import random
+
+numero = random.randint(1,10)
+contador = 5
+
+while contador >= 1:
+    print('JOGO DE ADIVINHA 🕹️')
+    print(f'Atenção!!! Você tem {contador} chances restantes.')
+    numero_digitado = int(input('Tente acertar o número que estou pensando entre 1 e 10: '))
+    print('-' * 60)
+        
+    if numero_digitado > 10 or numero_digitado < 1:
+        print('Errado 😞 digite um número entre 1 e 10.')
+        print('-' * 60)
+        contador -= 1
+        continue
+      
+    elif numero_digitado != numero:
+        print('Errado 😞 tente novamente!')
+        print('-' * 60)
+        contador -= 1
+        continue
+        
+    else:
+        print(f'Parabéns 🤩 você acertou! O número que pensei foi: {numero}')
+        print('-' * 60)
+        break
+
+if contador == 0:
+    print('Você não tem mais tentativas 😞')
+    print('-' * 60)
